0ccc675ada234927deb872f63fdac463f02a98c4
kirill panasyuk <kpmcpv@gmail.com>
2025-05-02 05:09:28 +0300

    added docker running, venv, requirements.txt

--- config_reader.py
+++ config_reader.py
@@ -19,7 +19,4 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
 
 
-# При импорте файла сразу создастся
-# и провалидируется объект конфига,
-# который можно далее импортировать из разных мест
 config = Settings()

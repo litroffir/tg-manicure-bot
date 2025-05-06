2889b7e319ed1fc787bf2e1f09c38b2edc6e3d9a
krff <git@amvera.ru>
2025-05-06 10:37:27 +0000

    deleted utils

--- utils/dates.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime, timedelta
-
-
-def generate_dates():
-    return [
-        (datetime.now() + timedelta(days=i)).strftime("%d.%m %H:%M")
-        for i in range(1, 8)
-    ]
